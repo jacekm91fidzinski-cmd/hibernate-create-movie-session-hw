8d8fc42e21f9653501305515424db167f2a994da
jacekm91fidzinski-cmd <jacekm91fidzinski@gmail.com>
2026-06-10 17:42:07 +0200

    Implement cinema hall and movie session functionality

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,62 @@
 package mate.academy;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    private static final Injector injector =
+            Injector.getInstance("mate.academy");
 
-        Movie fastAndFurious = new Movie("Fast and Furious");
-        fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
-        movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
-        movieService.getAll().forEach(System.out::println);
+    public static void main(String[] args) {
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+
+        CinemaHallService cinemaHallService =
+                (CinemaHallService) injector.getInstance(
+                        CinemaHallService.class);
+
+        MovieSessionService movieSessionService =
+                (MovieSessionService) injector.getInstance(
+                        MovieSessionService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("Avatar");
+        movie.setDescription("Sci-fi");
+
+        movie = movieService.add(movie);
+
+        System.out.println(movieService.get(movie.getId()));
+        System.out.println(movieService.getAll());
+
+        CinemaHall hall = new CinemaHall();
+        hall.setCapacity(120);
+        hall.setDescription("Blue hall");
+
+        hall = cinemaHallService.add(hall);
+
+        System.out.println(cinemaHallService.get(hall.getId()));
+        System.out.println(cinemaHallService.getAll());
+
+        MovieSession session = new MovieSession();
+        session.setMovie(movie);
+        session.setCinemaHall(hall);
+        session.setShowTime(LocalDateTime.now());
+
+        session = movieSessionService.add(session);
+
+        System.out.println(
+                movieSessionService.get(session.getId()));
+
+        System.out.println(
+                movieSessionService.findAvailableSessions(
+                        movie.getId(),
+                        LocalDate.now()));
     }
 }
